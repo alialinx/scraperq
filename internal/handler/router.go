08c59274c0baee3b30d5946e91b4bcbeb2805636
ali <ali.alin@makdos.com>
2026-03-05 17:33:45 +0300

    add router with all endpoints

--- internal/handler/router.go
+++ internal/handler/router.go
@@ -1,1 +1,14 @@
 package handler
+
+import "net/http"
+
+func SetupRoutes(authHandler *AuthHandler, jobHandler *JobHandler) *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/register", authHandler.Register)
+	mux.HandleFunc("/login", authHandler.Login)
+	mux.HandleFunc("/jobs", jobHandler.CreateJob)
+	mux.HandleFunc("/jobs/status", jobHandler.GetJobStatus)
+
+	return mux
+}
