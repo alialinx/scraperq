9db41c0aff12fcece64fea91979f1e57c3e048dd
ali <ali.alin@makdos.com>
2026-03-05 17:33:45 +0300

    add auth request response structs to user model

--- internal/models/user.go
+++ internal/models/user.go
@@ -12,3 +12,18 @@ type User struct {
 	MonthlyLimit int       `json:"monthly_limit"`
 	CreatedAt    time.Time `json:"created_at"`
 }
+
+type RegisterRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type AuthResponse struct {
+	Token  string `json:"token"`
+	APIKey string `json:"api_key,omitempty"`
+}
