772297cb74c2ed8ca3fe882bea2686e44db7b9bf
ali <ali.alin@makdos.com>
2026-03-05 12:36:30 +0300

    add http scraper with timeout

--- internal/scraper/http.go
+++ internal/scraper/http.go
@@ -1,1 +1,44 @@
 package scraper
+
+import (
+	"github.com/alialin/scraperq/internal/models"
+	"io"
+	"net/http"
+	"time"
+)
+
+type Scraper interface {
+	Scrape(url string) (*models.Result, error)
+}
+
+type HTTPScraper struct {
+	client *http.Client
+}
+
+func NewHTTPScraper() *HTTPScraper {
+	client := &http.Client{Timeout: 10 * time.Second}
+	return &HTTPScraper{client: client}
+}
+
+func (s *HTTPScraper) Scrape(url string) (*models.Result, error) {
+
+	resp, err := s.client.Get(url)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return &models.Result{
+		StatusCode: resp.StatusCode,
+		BodySize:   len(body),
+		ScrapedAt:  time.Now(),
+	}, nil
+
+}
